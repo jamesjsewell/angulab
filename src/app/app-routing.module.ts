b5063e02c05043464c59dead1398f20ad3695a69
james sewell <james.sewell@improving.com>
2021-05-29 14:20:51 -0500

    adds jokes and animals lazy loaded feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'animals',
+    loadChildren: () =>
+      import('./animals/animals.module').then((m) => m.AnimalsModule),
+  },
+  {
+    path: 'jokes',
+    loadChildren: () =>
+      import('./jokes/jokes.module').then((m) => m.JokesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/jokes/jokes/jokes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jokes',
+  templateUrl: './jokes.component.html',
+  styleUrls: ['./jokes.component.scss']
+})
+export class JokesComponent {
+
+}
